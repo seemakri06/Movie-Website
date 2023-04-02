7cd19f60ba1eba29dadd8f15dcaf5264335a9411
seemakri06 <seema.iit2020@gmail.com>
2023-04-02 14:48:45 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import Movies from "./Movies";
+import Search from "./Search";
+
+const Home = () => {
+  // const name = useGlobalContext();
+  return (
+    <>
+      <Search />
+      <Movies />
+    </>
+  );
+};
+
+export default Home;
